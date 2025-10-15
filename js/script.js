2652787b0d53f348d1864aebba598c43749d2e88
Ahmed Atef <ahmedatef15t@gmail.com>
2025-10-15 17:28:35 +0300

    update

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const header=document.querySelector('.header');
+const navEl=document.querySelector('.btn-mobile-nav')
+navEl.addEventListener('click',function(){
+header.classList.toggle('nav-open');
+})
+// sticy
+const sectionheroEl=document.querySelector(".section-hero");
+const obs=new IntersectionObserver(function(entries){
+const ent=entries[0];
+if(ent.isIntersecting === false){
+     document.body.classList.add('sticky')
+}
+   
+if(!ent.isIntersecting === false){
+     document.body.classList.remove('sticky')
+}
+   
+},{
+root:null,
+threshold:0,
+rootMargin:'-80px'
+})
+obs.observe(sectionheroEl);
